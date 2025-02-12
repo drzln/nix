2e42a314b795615bf96c07001cf98535ea8c342b
luis <luis@pleme.io>
2025-02-12 00:20:44 -0300

    add rubocop to format ruby in conform

--- modules/home-manager/blackmatter/components/nvim/plugins/stevearc/conform.nvim/config.lua
+++ modules/home-manager/blackmatter/components/nvim/plugins/stevearc/conform.nvim/config.lua
@@ -4,12 +4,7 @@ function M.setup()
 		formatters_by_ft = {
 			lua = { "stylua" },
 			nix = { "alejandra" },
-			-- Conform will run multiple formatters sequentially
-			-- python = { "isort", "black" },
-			-- You can customize some of the format options for the filetype (:help conform.format)
-			-- rust = { "rustfmt", lsp_format = "fallback" },
-			-- Conform will run the first available formatter
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			ruby = { "rubocop" },
 		},
 	})
 end
