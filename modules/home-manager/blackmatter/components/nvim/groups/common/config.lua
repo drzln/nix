fa4948c093d90059cc1e750480f46ed2746df0aa
luis <luis@pleme.io>
2025-02-14 13:21:32 -0300

    move project.nvim configuration

--- modules/home-manager/blackmatter/components/nvim/groups/common/config.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/common/config.lua
@@ -5,7 +5,7 @@ function M.setup()
 
   -- highlight search results
   vim.opt.hlsearch = true
-  
+
   -- customize file finding
   vim.opt.path:remove '/usr/include'
   vim.opt.path:append '**'
@@ -48,6 +48,13 @@ function M.setup()
 
   -- required for theming but cannot be in theming
   vim.opt.termguicolors = true
+
+  -- projects
+	require('project_nvim').setup {
+		manual_mode = false,
+		detection_methods = { 'lsp', 'pattern' },
+		patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
+	};
 end
 
 return M
