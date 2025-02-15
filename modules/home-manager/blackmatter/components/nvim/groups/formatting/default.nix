965c12f042fe5abff156db0446169d7636309a6a
luis <luis@pleme.io>
2025-02-15 09:15:59 -0300

    place back the conform configuration

--- modules/home-manager/blackmatter/components/nvim/groups/formatting/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/formatting/default.nix
@@ -1,31 +1,31 @@
-{ config, lib, ... }:
-with lib;
-let
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
   cfg = config.blackmatter.components.nvim.plugin.groups.formatting;
   common = import ../../common;
   configPath = "${common.includesPath}/formatting/config.lua";
-in
-{
-  options.blackmatter.components.nvim.plugin.groups.formatting =
-    {
-      enable = mkEnableOption "manage formatting";
-    };
+in {
+  options.blackmatter.components.nvim.plugin.groups.formatting = {
+    enable = mkEnableOption "manage formatting";
+  };
 
   imports = [
     ../../plugins/stevearc/conform.nvim
   ];
 
-  config =
-    mkMerge [
-      (mkIf cfg.enable
-        {
-          home.file."${configPath}".source = ./config.lua;
-          blackmatter.components.nvim.plugins =
-            {
-							# formatting framework
-              stevearc."conform.nvim".enable = true;
-            };
-        }
-      )
-    ];
+  config = mkMerge [
+    (
+      mkIf cfg.enable
+      {
+        home.file."${configPath}".source = ./config.lua;
+        blackmatter.components.nvim.plugins = {
+          # formatting framework
+          stevearc."conform.nvim".enable = true;
+        };
+      }
+    )
+  ];
 }
