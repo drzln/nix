13b2e5b9499522527cd267fbce2bfb75fe75e9bb
luis <luis@pleme.io>
2025-02-20 04:41:42 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/conf/lua/lsp/config/cmake.lua
+++ modules/home-manager/blackmatter/components/nvim/conf/lua/lsp/config/cmake.lua
@@ -4,10 +4,7 @@ function M.setup(opts)
 	local lsputils = require("lsp.utils")
 
 	-- override cmake
-	local cmake_opts = lsputils.merge(
-		{},
-		opts
-	)
+	local cmake_opts = lsputils.merge({}, opts)
 
 	-- enable cmake
 	require("lspconfig").cmake.setup(cmake_opts)
