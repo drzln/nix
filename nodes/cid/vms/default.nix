90bab518e1ba694589103a3f9295247f61f8936c
luis <ldesiqueira@pinger.com>
2025-02-26 11:34:28 -0300

    init

--- nodes/cid/vms/default.nix
+++ nodes/cid/vms/default.nix
@@ -1,6 +1,12 @@
 {...}: {
   nix = {
-    settings.trusted-users = ["ldesiqueira"]; # Replace with your macOS username
-    linux-builder.enable = true;
+    settings.trusted-users = ["ldesiqueira"];
+    linux-builder = {
+      enable = true;
+      config = {
+        virtualisation.darwin-builder.user = "ldesiqueira";
+        virtualisation.darwin-builder.hostPort = 22;
+      };
+    };
   };
 }
