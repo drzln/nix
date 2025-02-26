a9cde40616031e0051fcab976a59bcec0e511759
luis <ldesiqueira@pinger.com>
2025-02-26 11:40:31 -0300

    init

--- nodes/cid/vms/default.nix
+++ nodes/cid/vms/default.nix
@@ -4,6 +4,9 @@
     linux-builder = {
       enable = true;
       ephemeral = true;
+      config = {pkgs, ...}: {
+        environment.systemPackages = with pkgs; [neovim];
+      };
       # config = {
       #   virtualisation.darwin-builder.user = "ldesiqueira";
       #   virtualisation.darwin-builder.hostPort = 22;
