d619a2b52dc5d234d2c1d011bc82b85fa34868cb
luis <ldesiqueira@pinger.com>
2025-02-28 12:56:33 -0300

    add a main vm

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -1,4 +1,21 @@
-{ nixpkgs, specialArgs, home-manager, sops-nix }: {
+{
+  nixpkgs,
+  specialArgs,
+  home-manager,
+  sops-nix,
+}: {
+  kid = nixpkgs.lib.nixosSystem {
+    inherit specialArgs;
+    system = "aarch64-linux";
+    modules = [
+      ../vms/main.nix
+      # /etc/nixos/configuration.nix
+      # ../nodes/plo
+      # home-manager.nixosModules.home-manager
+      # sops-nix.nixosModules.sops
+    ];
+  };
+
   plo = nixpkgs.lib.nixosSystem {
     inherit specialArgs;
     system = "x86_64-linux";
