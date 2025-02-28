348f70f45a40bca414bf958dd1e88b76160d7885
luis <ldesiqueira@pinger.com>
2025-02-28 20:30:25 -0300

    trust me bro

--- flake.nix
+++ flake.nix
@@ -90,6 +90,13 @@
           }
           // specialArgs;
         modules = [
+          {
+            # Pin nixpkgs to the flake input, so that the packages installed
+            # come from the flake inputs.nixpkgs.url.
+            nix.registry.nixpkgs.flake = nixpkgs;
+            # set disk size to to 20G
+            virtualisation.diskSize = 20 * 1024;
+          }
           ./vms/main.nix
         ];
       };
