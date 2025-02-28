ca00308c670a7a26c34822e1ed3d2931aa88fa24
luis <ldesiqueira@pinger.com>
2025-02-28 13:53:59 -0300

    add a main vm

--- nodes/cid/vms/default.nix
+++ nodes/cid/vms/default.nix
@@ -1,21 +1,21 @@
 {...}: {
-  nix = {
-    settings.trusted-users = ["ldesiqueira"];
-    settings.extra-trusted-users = ["ldesiqueira"];
-    linux-builder = {
-      enable = true;
-      ephemeral = true;
-      config = {pkgs, ...}: {
-        system.stateVersion = "24.05";
-        # environment.systemPackages = with pkgs; [vim];
-        virtualisation = {
-          darwin-builder = {
-            diskSize = 40 * 1024;
-            memorySize = 8 * 1024;
-          };
-          cores = 6;
-        };
-      };
-    };
-  };
+  # nix = {
+  #   settings.trusted-users = ["ldesiqueira"];
+  #   settings.extra-trusted-users = ["ldesiqueira"];
+  #   linux-builder = {
+  #     enable = true;
+  #     ephemeral = true;
+  #     config = {pkgs, ...}: {
+  #       system.stateVersion = "24.05";
+  #       # environment.systemPackages = with pkgs; [vim];
+  #       virtualisation = {
+  #         darwin-builder = {
+  #           diskSize = 40 * 1024;
+  #           memorySize = 8 * 1024;
+  #         };
+  #         cores = 6;
+  #       };
+  #     };
+  #   };
+  # };
 }
