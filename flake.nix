aca2508a8cd61caa5ccc90f8dd50f25a5acfa39b
luis <ldesiqueira@pinger.com>
2025-03-01 21:28:14 -0300

    trust me bro

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
     ...
   } @ inputs: let
     inherit inputs self;
+
     overlays =
       import ./overlays/default.nix
       ++ builtins.attrValues sops-nix.overlays
@@ -54,136 +55,32 @@
         (final: prev: {nixhashsync = nixhashsync.packages.${prev.system}.default;})
       ];
 
+    mkPkgs = system:
+      import nixpkgs {
+        inherit system overlays;
+        config.allowUnfree = true;
+      };
+
     requirements = {inherit inputs self;};
 
     specialArgs = {
       inherit requirements packages;
     };
 
-    nixosConfigurations =
-      import ./nixosConfigurations {
-        inherit nixpkgs home-manager sops-nix specialArgs;
-      }
-      // {
-        kid-qcow2 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            ({
-              modulesPath,
-              config,
-              lib,
-              pkgs,
-              ...
-            }: {
-              imports = [
-                "${toString modulesPath}/profiles/qemu-guest.nix"
-              ];
-              system.stateVersion = "25.05";
+    nixosConfigurations = import ./nixosConfigurations {
+      inherit nixpkgs home-manager sops-nix specialArgs;
+    };
 
-              fileSystems."/" = {
-                device = "/dev/disk/by-label/nixos";
-                autoResize = true;
-                fsType = "ext4";
-              };
-
-              boot.kernelParams = ["console=ttyS0"];
-              boot.loader.grub.device = lib.mkDefault "/dev/vda";
-
-              system.build.qcow2 = import "${modulesPath}/../lib/make-disk-image.nix" {
-                inherit lib config pkgs;
-                diskSize = 10240;
-                format = "qcow2";
-                partitionTableType = "hybrid";
-              };
-            })
-
-            # ./vms/test.nix
-            # ({modulesPath, ...}: {
-            #   imports = [(modulesPath + "/virtualisation/qemu-vm.nix")];
-            # })
-          ];
-        };
-      };
-
-    mkPkgs = system:
-      import nixpkgs {
-        inherit system overlays;
-        crossSystem = {
-          config = "aarch64-linux";
-        };
-        config.allowUnfree = true;
-      };
     nixos-modules = import ./modules/nixos;
+
     base-packages = flake-utils.lib.eachDefaultSystem (system: let
       pkgs = mkPkgs system;
     in {
       neovim = pkgs.callPackage ./packages/neovim {};
       nixhashsync = nixhashsync.packages.${system}.default;
-      # kid-qcow2 = self.nixosConfigurations.kid-qcow2.config.system.build.qcow2;
-      # kid-qcow2 = packages.kid-qcow2-configuration.config.system.build.qcow2Image;
-      # kid-qcow2-configuration = nixpkgs.lib.nixosSystem {
-      #   inherit system;
-      #   modules = [
-      #     ./vms/test.nix
-      #     ({modulesPath, ...}: {
-      #       imports = [(modulesPath + "/virtualisation/qemu-vm.nix")];
-      #       virtualisation = {
-      #         diskImageFormat = "qcow2";
-      #         diskSize = 8192; # 8GB
-      #         memorySize = 2048; # 2GB RAM
-      #       };
-      #     })
-      #   ];
-      # };
     });
-    # aarch64-linux-pkgs = mkPkgs "aarch64-linux";
-    aarch64-darwin-pkgs = mkPkgs "aarch64-darwin";
     packages =
-      base-packages
-      // {
-        kid.aarch64-darwin = aarch64-darwin-pkgs.runCommand "run-kid" {} ''
-          mkdir -p $out/bin
-
-          # Store paths for QEMU binaries
-          QEMU_IMG="${aarch64-darwin-pkgs.qemu}/bin/qemu-img"
-          QEMU_SYSTEM="${aarch64-darwin-pkgs.qemu}/bin/qemu-system-aarch64"
-          BASE_PATH=./etc/nixos/vm/main
-          QCOW=$out/nixos-vm.qcow2
-          SIZE=100G
-          MEM=4096
-
-          cat > $out/bin/run-kid << EOF
-          #!/bin/sh
-          set -e
-          nix build ".#packages.kid-image.aarch64-linux" --extra-platforms aarch64-darwin
-          sudo ln -sf ./result/nixos-vm.qcow2 $QCOW
-
-          # Create the disk image if it doesn't exist
-          ! [ -d $BASE_PATH ] && mkdir -p $BASE_PATH
-
-          # Run QEMU with hardcoded store paths
-          echo "starting qemu VM kid"
-          echo "$SIZE"
-          echo "$MEM"
-          echo "$QCOW"
-          echo "$QEMU_IMG"
-          echo "$QEMU_SYSTEM"
-          echo "starting qemu VM kid"
-          $QEMU_SYSTEM \
-            -machine virt \
-            -accel hvf \
-            -cpu host \
-            -m $MEM \
-            -smp 2 \
-            -drive file=$QCOW,if=virtio \
-            -netdev user,id=net0,hostfwd=tcp::2222-:22,net=192.168.50.0/24,dhcpstart=192.168.50.10 \
-            -device virtio-net-pci,netdev=net0 \
-            -nographic \
-            -D qemu.log -d guest_errors,unimp
-          EOF
-          chmod +x $out/bin/run-kid
-        '';
-      };
+      base-packages;
   in {
     inherit nixosConfigurations packages;
 
@@ -202,9 +99,6 @@
         home-manager
         inputs
         ;
-      # extraSpecialArgs = specialArgs;
-      # pkgs = mkPkgs "x86_64-darwin";
-      darwin-pkgs = mkPkgs "x86_64-darwin";
     };
   };
 }
