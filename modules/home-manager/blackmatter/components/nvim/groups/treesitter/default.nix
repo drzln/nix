629dec5f108fe703ceb1e94ae1ba5dc460012501
luis <ldesiqueira@pinger.com>
2025-03-03 14:31:41 -0300

    let treesitter determine the commentstring

--- modules/home-manager/blackmatter/components/nvim/groups/treesitter/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/treesitter/default.nix
@@ -1,18 +1,20 @@
-{ config, lib, ... }:
-with lib;
-let
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
   cfg = config.blackmatter.components.nvim.plugin.groups.treesitter;
   common = import ../../common;
   configPath = "${common.includesPath}/treesitter/config.lua";
-in
-{
-  options.blackmatter.components.nvim.plugin.groups.treesitter =
-    {
-      enable = mkEnableOption "treesitter";
-    };
+in {
+  options.blackmatter.components.nvim.plugin.groups.treesitter = {
+    enable = mkEnableOption "treesitter";
+  };
 
   imports = [
     ../../plugins/nvim-treesitter/nvim-treesitter
+    ../../plugins/JoosepAlviste/nvim-ts-context-commentstring
     # ../../plugins/nvim-treesitter/playground
     # ../../plugins/nvim-treesitter/nvim-treesitter-context
     # ../../plugins/nvim-treesitter/nvim-treesitter-refactor
@@ -21,30 +23,28 @@ in
     # ../../plugins/p00f/nvim-ts-rainbow
     # ../../plugins/RRethy/nvim-treesitter-endwise
     # ../../plugins/RRethy/nvim-treesitter-textsubjects
-    # ../../plugins/JoosepAlviste/nvim-ts-context-commentstring
   ];
 
-  config =
-    mkMerge [
-      (mkIf cfg.enable
-        {
-          home.file."${configPath}".source = ./config.lua;
-          blackmatter.components.nvim.plugins =
-            {
-              nvim-treesitter.nvim-treesitter.enable = true;
-              # nvim-treesitter.playground.enable = true;
-              # nvim-treesitter.nvim-treesitter-context.enable = false;
-              # nvim-treesitter.nvim-treesitter-refactor.enable = true;
-              # nvim-treesitter.nvim-treesitter-textobjects.enable = false;
-              # windwp.nvim-ts-autotag.enable = false;
-              # now a part of nvim-treesitter as config
-              # p00f.nvim-ts-rainbow.enable = false;
-              # RRethy.nvim-treesitter-endwise.enable = true;
-              # RRethy.nvim-treesitter-textsubjects.enable = true;
-              # also merged into native nvim-treesitter so disabled now
-              # JoosepAlviste.nvim-ts-context-commentstring.enable = false;
-            };
-        }
-      )
-    ];
+  config = mkMerge [
+    (
+      mkIf cfg.enable
+      {
+        home.file."${configPath}".source = ./config.lua;
+        blackmatter.components.nvim.plugins = {
+          nvim-treesitter.nvim-treesitter.enable = true;
+          JoosepAlviste.nvim-ts-context-commentstring.enable = true;
+          # nvim-treesitter.playground.enable = true;
+          # nvim-treesitter.nvim-treesitter-context.enable = false;
+          # nvim-treesitter.nvim-treesitter-refactor.enable = true;
+          # nvim-treesitter.nvim-treesitter-textobjects.enable = false;
+          # windwp.nvim-ts-autotag.enable = false;
+          # now a part of nvim-treesitter as config
+          # p00f.nvim-ts-rainbow.enable = false;
+          # RRethy.nvim-treesitter-endwise.enable = true;
+          # RRethy.nvim-treesitter-textsubjects.enable = true;
+          # also merged into native nvim-treesitter so disabled now
+        };
+      }
+    )
+  ];
 }
--- modules/home-manager/blackmatter/components/nvim/plugins/numToStr/Comment.nvim/default.nix
+++ modules/home-manager/blackmatter/components/nvim/plugins/numToStr/Comment.nvim/default.nix
@@ -1,25 +1,27 @@
-{ lib, config, ... }:
-with lib;
-let
-	cfg = config.blackmatter.components.nvim.plugins.${author}.${name};
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.blackmatter.components.nvim.plugins.${author}.${name};
   common = import ../../../common;
   url = "${common.baseRepoUrl}/${author}/${name}";
   plugPath = "${common.basePlugPath}/${author}/start/${name}";
   configPath = "${common.includesPath}/${author}/${plugName}.lua";
   author = "numToStr";
   name = "Comment.nvim";
-	plugName = "Comment";
+  plugName = "Comment";
   ref = "master";
   rev = import ./rev.nix;
-in
-{
+in {
   options.blackmatter.components.nvim.plugins.${author}.${name}.enable =
     mkEnableOption "${author}/${name}";
 
   config = mkMerge [
     (mkIf cfg.enable {
       home.file."${plugPath}".source =
-        builtins.fetchGit { inherit ref rev url; };
+        builtins.fetchGit {inherit ref rev url;};
     })
   ];
 }
