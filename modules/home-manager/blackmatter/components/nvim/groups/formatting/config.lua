b67e2356afd270d6a7d13326a65998475b2d424d
luis <ldesiqueira@pinger.com>
2025-03-03 14:42:33 -0300

    add terraform formatting

--- modules/home-manager/blackmatter/components/nvim/groups/formatting/config.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/formatting/config.lua
@@ -2,9 +2,12 @@ local M = {}
 function M.setup()
 	require("conform").setup({
 		formatters_by_ft = {
+			["terraform-vars"] = { "terraform_fmt" },
+			terraform = { "terraform_fmt" },
 			typescript = { "prettier" },
 			javascript = { "prettier" },
 			markdown = { "prettier" },
+			tf = { "terraform_fmt" },
 			json = { "prettier" },
 			nix = { "alejandra" },
 			html = { "prettier" },
