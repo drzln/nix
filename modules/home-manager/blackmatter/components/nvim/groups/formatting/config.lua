94fe998c2176ee948f75a08d919e2420506eb374
luis <ldesiqueira@pinger.com>
2025-03-19 20:09:15 -0300

    add google-java-format to lsp tools

--- modules/home-manager/blackmatter/components/nvim/groups/formatting/config.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/formatting/config.lua
@@ -3,6 +3,7 @@ function M.setup()
 	require("conform").setup({
 		formatters_by_ft = {
 			["terraform-vars"] = { "terraform_fmt" },
+			java = { "google_java_format" },
 			terraform = { "terraform_fmt" },
 			typescript = { "prettier" },
 			javascript = { "prettier" },
