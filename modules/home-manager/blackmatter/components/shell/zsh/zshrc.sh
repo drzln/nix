070d74f5f286eeda906a42d4b789181cdc907dd9
luis <ldesiqueira@pinger.com>
2025-03-23 05:05:24 -0300

    add stuff

--- modules/home-manager/blackmatter/components/shell/zsh/zshrc.sh
+++ modules/home-manager/blackmatter/components/shell/zsh/zshrc.sh
@@ -5,15 +5,16 @@
 ! [ -d ~/.config/shellz ] && mkdir -p ~/.config/shellz
 
 function load_shellz_mod() {
-	[ -d ~/.config/shellz/"$1" ] && source ~/.config/shellz/$1/main.sh
+  [ -d ~/.config/shellz/"$1" ] && source ~/.config/shellz/$1/main.sh
 }
 
 declare -a shellz_mods=(
-	direnv
+  direnv
+  fzf
 )
 
 for m in "${shellz_mods[@]}"; do
-	load_shellz_mod $m
+  load_shellz_mod $m
 done
 
 # vim movement
