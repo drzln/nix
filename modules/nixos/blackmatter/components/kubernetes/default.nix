d9a67f23ac118fa019544bb1da32bb410223dae0
luis <luis@pleme.io>
2025-04-25 20:21:45 -0300

    init

--- modules/nixos/blackmatter/components/kubernetes/default.nix
+++ modules/nixos/blackmatter/components/kubernetes/default.nix
@@ -11,22 +11,22 @@ with lib;
 ############################################################
   let
     cfg = config.blackmatter.components.kubernetes;
-    overlay = cfg.overlay or inputs.nix-kubernetes.overlays.default;
+    # overlay = cfg.overlay or inputs.nix-kubernetes.overlays.default;
 
-    pkgs' = import inputs.nixpkgs {
-      system = pkgs.system;
-      overlays = [overlay];
-    };
+    # pkgs' = import inputs.nixpkgs {
+    #   system = pkgs.system;
+    #   overlays = [overlay];
+    # };
 
-    k8sPkgs =
-      pkgs'.kubernetesPackages
-      // {
-        kube-apiserver = pkgs'.kube-apiserver;
-        kube-controller-manager = pkgs'.kube-controller-manager;
-        kube-scheduler = pkgs'.kube-scheduler;
-      };
+    # k8sPkgs =
+    #   pkgs'.kubernetesPackages
+    #   // {
+    #     kube-apiserver = pkgs'.kube-apiserver;
+    #     kube-controller-manager = pkgs'.kube-controller-manager;
+    #     kube-scheduler = pkgs'.kube-scheduler;
+    #   };
 
-    etcdPkg = cfg.etcdPackage or pkgs'.etcd;
+    # etcdPkg = cfg.etcdPackage or pkgs'.etcd;
     containerdPkg = cfg.containerdPackage or pkgs'.containerd;
 
     isMaster = cfg.role == "master" || cfg.role == "single";
@@ -121,75 +121,75 @@ with lib;
 
         ################ containerd ##############
         (mkIf isWorker {
-          services.containerd = {
-            enable = true;
-            package = containerdPkg;
-            settings.plugins."io.containerd.grpc.v1.cri".systemdCgroup = true;
-          };
+          # services.containerd = {
+          #   enable = true;
+          #   package = containerdPkg;
+          #   settings.plugins."io.containerd.grpc.v1.cri".systemdCgroup = true;
+          # };
         })
 
         ################ master role #############
         (mkIf isMaster {
-          services.kubernetes = {
-            roles = ["master"];
-
-            kubelet.package = k8sPkgs.kubelet;
-            apiserver.package = k8sPkgs.kube-apiserver;
-            controllerManager.package = k8sPkgs.kube-controller-manager;
-            scheduler.package = k8sPkgs.kube-scheduler;
-
-            etcd = {
-              enable = true;
-              package = etcdPkg;
-            };
-
-            kubelet.extraOpts = ''
-              --container-runtime-endpoint=unix:///run/containerd/containerd.sock
-              ${cfg.extraKubeletOpts}
-            '';
-
-            kubeadm.extraConfig =
-              lib.recursiveUpdate
-              {
-                apiServer.extraArgs =
-                  cfg.extraApiArgs // {"service-node-port-range" = cfg.nodePortRange;};
-              }
-              cfg.kubeadmExtra;
-          };
+          # services.kubernetes = {
+          #   roles = ["master"];
+          #
+          #   kubelet.package = k8sPkgs.kubelet;
+          #   apiserver.package = k8sPkgs.kube-apiserver;
+          #   controllerManager.package = k8sPkgs.kube-controller-manager;
+          #   scheduler.package = k8sPkgs.kube-scheduler;
+          #
+          #   etcd = {
+          #     enable = true;
+          #     package = etcdPkg;
+          #   };
+          #
+          #   kubelet.extraOpts = ''
+          #     --container-runtime-endpoint=unix:///run/containerd/containerd.sock
+          #     ${cfg.extraKubeletOpts}
+          #   '';
+          #
+          #   kubeadm.extraConfig =
+          #     lib.recursiveUpdate
+          #     {
+          #       apiServer.extraArgs =
+          #         cfg.extraApiArgs // {"service-node-port-range" = cfg.nodePortRange;};
+          #     }
+          #     cfg.kubeadmExtra;
+          # };
         })
 
         ############### worker role ##############
         (mkIf (isWorker && !isMaster) {
-          services.kubernetes = {
-            roles = ["node"];
-            kubelet.package = k8sPkgs.kubelet;
-            kubelet.extraOpts = ''
-              --container-runtime-endpoint=unix:///run/containerd/containerd.sock
-              ${cfg.extraKubeletOpts}
-            '';
-            kubeadm.join = {
-              enable = true;
-              address = cfg.join.address;
-              token = cfg.join.token;
-              caCertHash = cfg.join.caHash;
-            };
-          };
+          # services.kubernetes = {
+          #   roles = ["node"];
+          #   kubelet.package = k8sPkgs.kubelet;
+          #   kubelet.extraOpts = ''
+          #     --container-runtime-endpoint=unix:///run/containerd/containerd.sock
+          #     ${cfg.extraKubeletOpts}
+          #   '';
+          #   kubeadm.join = {
+          #     enable = true;
+          #     address = cfg.join.address;
+          #     token = cfg.join.token;
+          #     caCertHash = cfg.join.caHash;
+          #   };
+          # };
 
-          services.containerd = {
-            enable = true;
-            package = containerdPkg;
-            settings.plugins."io.containerd.grpc.v1.cri".systemdCgroup = true;
-          };
+          # services.containerd = {
+          #   enable = true;
+          #   package = containerdPkg;
+          #   settings.plugins."io.containerd.grpc.v1.cri".systemdCgroup = true;
+          # };
         })
 
         ######## build info for debugging ########
         {
-          environment.etc."k8s-build-info".text = ''
-            role=${cfg.role}
-            k8s=${k8sPkgs.kubelet.version}
-            etcd=${etcdPkg.version}
-            containerd=${containerdPkg.version}
-          '';
+          # environment.etc."k8s-build-info".text = ''
+          #   role=${cfg.role}
+          #   k8s=${k8sPkgs.kubelet.version}
+          #   etcd=${etcdPkg.version}
+          #   containerd=${containerdPkg.version}
+          # '';
         }
       ]);
     }
