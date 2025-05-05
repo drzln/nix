38163f759a46e24f740a94f19612f2ce07e5061a
luis <luis@pleme.io>
2025-05-05 05:48:00 -0300

    init

--- users/luis/plo/secrets/openai.nix
+++ users/luis/plo/secrets/openai.nix
@@ -1,6 +1,7 @@
+# users/luis/plo/secrets/openai.nix
 {...}: {
-  sops.secrets."openai/pinger/token" = {
-    sopsFile = ../../../../secrets.yaml;
-    path = "/home/luis/.config/openai/pinger/token";
-  };
+  # sops.secrets."openai/pinger/token" = {
+  #   sopsFile = ../../../../secrets.yaml;
+  #   path = "/home/luis/.config/openai/pinger/token";
+  # };
 }
