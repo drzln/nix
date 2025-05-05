a5d248dfa4b7db56840690d825002364651d5217
luis <luis@pleme.io>
2025-05-05 03:37:13 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/telescope/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/telescope/default.nix
@@ -1,13 +1,15 @@
-{ config, lib, pkgs, ... }:
-with lib;
-let
-  cfg = config.blackmatter.components.nvim.plugin.groups.telescope;
-in
 {
-  options.blackmatter.components.nvim.plugin.groups.telescope =
-    {
-      enable = mkEnableOption "telescope";
-    };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.blackmatter.components.nvim.plugin.groups.telescope;
+in {
+  options.blackmatter.components.nvim.plugin.groups.telescope = {
+    enable = mkEnableOption "telescope";
+  };
 
   imports = [
     ../../plugins/nvim-telescope/telescope.nvim
@@ -18,21 +20,20 @@ in
     ../../plugins/danielpieper/telescope-tmuxinator.nvim
   ];
 
-  config =
-    mkMerge [
-      (mkIf cfg.enable
-        {
-          blackmatter.components.nvim.plugins =
-            {
-              nvim-telescope."telescope.nvim".enable = true;
-              nvim-telescope."telescope-file-browser.nvim".enable = false;
-              nvim-telescope."telescope-project.nvim".enable = false;
-              nvim-telescope."telescope-dap.nvim".enable = false;
-              nvim-telescope."telescope-z.nvim".enable = false;
-              # TODO: off due to sandbox error
-              # danielpieper."telescope-tmuxinator.nvim".enable = false;
-            };
-        }
-      )
-    ];
+  config = mkMerge [
+    (
+      mkIf cfg.enable
+      {
+        blackmatter.components.nvim.plugins = {
+          nvim-telescope."telescope.nvim".enable = true;
+          nvim-telescope."telescope-file-browser.nvim".enable = false;
+          nvim-telescope."telescope-project.nvim".enable = false;
+          nvim-telescope."telescope-dap.nvim".enable = false;
+          nvim-telescope."telescope-z.nvim".enable = false;
+          # TODO: off due to sandbox error
+          # danielpieper."telescope-tmuxinator.nvim".enable = false;
+        };
+      }
+    )
+  ];
 }
