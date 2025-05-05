e0908c204699fe82dfd180fba7a18866bba627f6
luis <luis@pleme.io>
2025-05-05 03:57:00 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/default.nix
+++ modules/home-manager/blackmatter/components/nvim/default.nix
@@ -8,7 +8,6 @@ with lib; let
   cfg = config.blackmatter.components.nvim;
 in {
   imports = [./groups];
-
   options.blackmatter.components.nvim = {
     enable = mkEnableOption "enable neovim configuration";
     package = mkOption {
@@ -17,17 +16,11 @@ in {
       description = mdDoc "neovim configuration management";
     };
   };
-
   config = mkMerge [
     (mkIf cfg.enable {
-      # include the neovim package
       home.packages = [cfg.package];
-
-      # load a minimal set of utilities so you can play legos
       xdg.configFile."nvim/init.lua".source = ./conf/init.lua;
       xdg.configFile."nvim/lua/utils".source = ./conf/lua/utils;
-
-      # all other behaviors controlled by groups
       blackmatter.components.nvim.plugin.groups.enable = true;
     })
   ];
