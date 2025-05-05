5312ee81df35c78b90be3e3c7eb1a2ee8729551a
luis <luis@pleme.io>
2025-05-05 04:55:43 -0300

    init

--- nodes/plo/boot.nix
+++ nodes/plo/boot.nix
@@ -1,11 +1,11 @@
-{ ... }: {
+{...}: {
   boot.kernel.sysctl = {
     "fs.file-max" = 2097152;
     "kernel.pid_max" = 4194304;
   };
-  boot.kernelParams = [ 
-    "nvidia-drm.modeset=1" 
-    "selinux=0" 
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "selinux=0"
     "apparmor=0"
   ];
   boot.loader.systemd-boot.enable = true;
