a76c8fa9493b469b2792ec7679a8cd77b75050b7
luis <luis@pleme.io>
2025-05-05 07:36:00 -0300

    init

--- nodes/plo/configuration.nix
+++ nodes/plo/configuration.nix
@@ -23,28 +23,23 @@
   };
 in {
   system.stateVersion = "24.05";
-
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowImportFromDerivation = true;
-
   imports = [
     requirements.inputs.self.nixosModules.blackmatter
   ];
   blackmatter.profiles.blizzard.enable = true;
-
   # makes /bin/bash work
   services.envfs.enable = true;
   environment.systemPackages = with pkgs; [
     kubectl
   ];
-
   users.users.luis =
     {
       uid = 1001;
       description = "luis";
     }
     // sudo-users-common;
-
   security.sudo.extraConfig = ''
     luis ALL=(ALL) NOPASSWD:ALL
   '';
