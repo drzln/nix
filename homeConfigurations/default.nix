c4e07495fdedcd195bf11442a28140cd2a7dab30
luis <luis@pleme.io>
2025-05-05 05:04:43 -0300

    init

--- homeConfigurations/default.nix
+++ homeConfigurations/default.nix
@@ -12,22 +12,4 @@
       ../users/luis/plo
     ];
   };
-
-  "gab@plo" = home-manager.lib.homeManagerConfiguration {
-    inherit extraSpecialArgs pkgs;
-    modules = [
-      sops-nix.homeManagerModules.sops
-      ../users/gab/plo
-    ];
-  };
-
-  "gaby@plo" = home-manager.lib.homeManagerConfiguration {
-    inherit extraSpecialArgs pkgs;
-    modules = [../users/gaby/plo];
-  };
-
-  "gabrielad@gcd" = home-manager.lib.homeManagerConfiguration {
-    inherit extraSpecialArgs pkgs;
-    modules = [../users/gabrielad/gcd];
-  };
 }
