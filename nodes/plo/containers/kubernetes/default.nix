1f32811835a9f4eb6b50c7d274d962a4729a0366
luis <luis@pleme.io>
2025-05-07 11:32:53 -0300

    init

--- nodes/plo/containers/kubernetes/default.nix
+++ nodes/plo/containers/kubernetes/default.nix
@@ -153,86 +153,6 @@
       // container.defaults;
     single =
       {
-        sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-        sops.secrets = {
-          "kubernetes/ca/crt" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/ca.crt";
-          };
-          "kubernetes/ca/key" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/ca.key";
-          };
-          "kubernetes/apiserver/crt" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/apiserver.crt";
-          };
-          "kubernetes/apiserver/key" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/apiserver.key";
-          };
-          "kubernetes/kubelet/crt" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/kubelet.crt";
-          };
-          "kubernetes/kubelet/key" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/kubelet.key";
-          };
-          "kubernetes/etcd/crt" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/etcd.crt";
-          };
-          "kubernetes/etcd/key" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/etcd.key";
-          };
-          "kubernetes/admin/crt" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/admin.crt";
-          };
-          "kubernetes/admin/key" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/admin.key";
-          };
-          "kubernetes/san/cnf" = {
-            mode = "0444";
-            owner = "root";
-            group = "root";
-            sopsFile = ../../../../secrets.yaml;
-            path = "/var/lib/blackmatter/pki/san.cnf";
-          };
-        };
         # bindMounts = {
         #   "/var/lib/blackmatter/pki" = {
         #     hostPath = "/var/lib/blackmatter/pki";
@@ -252,6 +172,87 @@
             blackmatter.components.kubernetes = {
               enable = true;
               role = "single";
+            };
+            sops.age.keyFile = builtins.toString "/var/lib/sops-nix/key.txt";
+            environment.etc."sops/age/keys.txt".source = /var/lib/sops-nix/key.txt;
+            sops.secrets = {
+              "kubernetes/ca/crt" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/ca.crt";
+              };
+              "kubernetes/ca/key" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/ca.key";
+              };
+              "kubernetes/apiserver/crt" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/apiserver.crt";
+              };
+              "kubernetes/apiserver/key" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/apiserver.key";
+              };
+              "kubernetes/kubelet/crt" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/kubelet.crt";
+              };
+              "kubernetes/kubelet/key" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/kubelet.key";
+              };
+              "kubernetes/etcd/crt" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/etcd.crt";
+              };
+              "kubernetes/etcd/key" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/etcd.key";
+              };
+              "kubernetes/admin/crt" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/admin.crt";
+              };
+              "kubernetes/admin/key" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/admin.key";
+              };
+              "kubernetes/san/cnf" = {
+                mode = "0444";
+                owner = "root";
+                group = "root";
+                sopsFile = ../../../../secrets.yaml;
+                path = "/var/lib/blackmatter/pki/san.cnf";
+              };
             };
           };
         };
