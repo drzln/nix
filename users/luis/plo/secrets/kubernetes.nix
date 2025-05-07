f1dd43f94aa3f35c1180fce7d2298136d8a1113b
luis <luis@pleme.io>
2025-05-07 10:19:02 -0300

    init

--- users/luis/plo/secrets/kubernetes.nix
+++ users/luis/plo/secrets/kubernetes.nix
@@ -2,47 +2,47 @@
 {...}: {
   sops.secrets = {
     "ca/cert" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/ca.crt";
     };
     "ca/key" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/ca.key";
     };
     "apiserver/cert" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/apiserver.crt";
     };
     "apiserver/key" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/apiserver.key";
     };
     "kubelet/cert" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/kubelet.crt";
     };
     "kubelet/key" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/kubelet.key";
     };
     "etcd/cert" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/etcd.crt";
     };
     "etcd/key" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/etcd.key";
     };
     "admin/cert" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/admin.crt";
     };
     "admin/key" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/admin.key";
     };
     "san/cnf" = {
-      sopsFile = ../../../../kubernetes.yaml;
+      sopsFile = ../../../../secrets.yaml;
       path = "/var/lib/blackmatter/pki/san.cnf";
     };
   };
