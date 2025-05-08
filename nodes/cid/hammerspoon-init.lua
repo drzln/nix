e3e7b6bc1c69eba9da293e959a614241e9858908
drzzln <drzzln@protonmail.com>
2025-05-08 08:55:56 -0300

    init

--- nodes/cid/hammerspoon-init.lua
+++ nodes/cid/hammerspoon-init.lua
@@ -3,10 +3,10 @@ local function run(cmd)
 	hs.execute(cmd, true)
 end
 
-local function dockIsAutoHidden()
-	local out = run([[defaults read com.apple.dock autohide || echo 0]])
-	return tonumber(out) == 1
-end
+-- local function dockIsAutoHidden()
+-- 	local out = run([[defaults read com.apple.dock autohide || echo 0]])
+-- 	return tonumber(out) == 1
+-- end
 
 ------------------------------------------------------------------
 -- Raycast helpers
@@ -15,33 +15,33 @@ local function showRaycast()
 	-- simulate the default Raycast shortcut: ⌘ + space
 	hs.eventtap.keyStroke({ "cmd" }, "space", 0)
 end
-
+hs.hotkey.bind({ "cmd" }, "space", showRaycast)
 ------------------------------------------------------------------
 -- Spotlight helpers
 ------------------------------------------------------------------
-local function showSpotlight()
-	-- simulate the default Spotlight shortcut: ⌘ + space
-	hs.eventtap.keyStroke({ "cmd" }, "space", 0)
-end
+-- local function showSpotlight()
+-- 	-- simulate the default Spotlight shortcut: ⌘ + space
+-- 	hs.eventtap.keyStroke({ "cmd" }, "space", 0)
+-- end
 
 ------------------------------------------------------------------
 -- Hot-keys
 ------------------------------------------------------------------
 -- Alt + Space  ➜ Spotlight
-hs.hotkey.bind({ "alt" }, "space", showSpotlight)
+-- hs.hotkey.bind({ "alt" }, "space", showSpotlight)
 
 -- Cmd + Space ➜ Spotlight  (works even if you disabled the macOS
 -- global shortcut and gave it to Raycast)
-hs.hotkey.bind({ "cmd" }, "space", showSpotlight)
+-- hs.hotkey.bind({ "cmd" }, "space", showSpotlight)
 
 -- Toggle auto-hide (⌃⌥⌘ D)
-hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "D", function()
-	if dockIsAutoHidden() then
-		run([[defaults write com.apple.dock autohide -bool false]])
-		hs.alert.show("Dock: always visible")
-	else
-		run([[defaults write com.apple.dock autohide -bool true]])
-		hs.alert.show("Dock: auto-hide")
-	end
-	run([[killall Dock]])
-end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "D", function()
+-- 	if dockIsAutoHidden() then
+-- 		run([[defaults write com.apple.dock autohide -bool false]])
+-- 		hs.alert.show("Dock: always visible")
+-- 	else
+-- 		run([[defaults write com.apple.dock autohide -bool true]])
+-- 		hs.alert.show("Dock: auto-hide")
+-- 	end
+-- 	run([[killall Dock]])
+-- end)
