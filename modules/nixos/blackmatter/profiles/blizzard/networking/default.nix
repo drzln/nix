9851c10198c8b6416296562d0a80f7704a59a660
luis <luis@pleme.io>
2025-05-10 08:48:14 -0300

    init

--- modules/nixos/blackmatter/profiles/blizzard/networking/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/networking/default.nix
@@ -1,11 +1,11 @@
-# modules/nixos/blackmatter/profiles/blizzard/meta/default.nix
+# modules/nixos/blackmatter/profiles/blizzard/networking/default.nix
 {...}: {
   networking.networkmanager.enable = true;
+  networking.wireless.interfaces = ["wlp0s20f3"];
   # networking.firewall.enable = false;
   # networking.firewall.extraCommands = ''
   #   ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
   # '';
-  networking.wireless.interfaces = ["wlp0s20f3"];
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
   services.openssh.settings.PasswordAuthentication = true;
