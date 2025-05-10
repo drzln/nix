63c033cbdf66d1481071dadde96c06579f66ca70
luis <luis@pleme.io>
2025-05-10 07:25:00 -0300

    new parts/darwin.nix

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -2,42 +2,12 @@
 {
   nixpkgs,
   specialArgs,
-  home-manager,
-  sops-nix,
-  # inputs,
-  ...
-}: let
-  finalSpecialArgs =
-    specialArgs
-    // {
-      inherit nixpkgs;
-      overlays = import ../overlays;
-    };
-in {
-  kid = nixpkgs.lib.nixosSystem {
-    specialArgs = finalSpecialArgs;
-    system = "aarch64-linux";
-    modules = [
-      ../vms/main.nix
-      ({...}: {
-        # put more system config here
-      })
-      # /etc/nixos/configuration.nix
-      # ../nodes/plo
-      # home-manager.nixosModules.home-manager
-      # sops-nix.nixosModules.sops
-    ];
-  };
-
+}: {
   plo = nixpkgs.lib.nixosSystem {
-    inherit specialArgs;
     system = "x86_64-linux";
+    specialArgs = specialArgs;
     modules = [
-      home-manager.nixosModules.home-manager
-      sops-nix.nixosModules.sops
-      # inputs.nix-kubernetes.nixosModules.kubernetes
-      /etc/nixos/configuration.nix
-      ../nodes/plo
+      ./plo/configuration.nix
     ];
   };
 }
