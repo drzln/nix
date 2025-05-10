f2d92a7e2c7603e856eb4326af1b6f12bec2a297
luis <luis@pleme.io>
2025-05-10 11:32:37 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/keybindings/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/keybindings/default.nix
@@ -6,7 +6,7 @@
 with lib; let
   cfg = config.blackmatter.components.nvim.plugin.groups.keybindings;
   common = import ../../common;
-  configPath = "${common.includesPath}/keybindings/config.lua";
+  configPath = "${common.includesPath}/keybindings/init.lua";
 in {
   options.blackmatter.components.nvim.plugin.groups.keybindings = {
     enable = mkEnableOption "keybindings";
@@ -19,7 +19,7 @@ in {
     (
       mkIf cfg.enable
       {
-        home.file."${configPath}".source = ./config.lua;
+        home.file."${configPath}".source = ./init.lua;
         blackmatter.components.nvim.plugins = {
           hrsh7th.nvim-cmp.enable = true;
         };
