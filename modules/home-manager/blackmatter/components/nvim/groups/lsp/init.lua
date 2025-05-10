68e3bd8f2f49f572ff3c5fb7d74fa0f2d9398a54
luis <luis@pleme.io>
2025-05-10 12:20:53 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/lsp/init.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/lsp/init.lua
@@ -1,10 +1,8 @@
 -- modules/home-manager/blackmatter/components/nvim/groups/lsp/config.lua
 local M = {}
-
 function M.setup()
 	require("groups.lsp.settings").apply()
 	require("groups.lsp.filetypes").setup()
 	require("groups.lsp.mason").setup()
 end
-
 return M
