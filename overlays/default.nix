a6fd5d0aad92b66d750a2bb9cb8873a61bf85fd4
luis <luis@pleme.io>
2025-05-10 07:22:11 -0300

    new parts/darwin.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,2 +1,11 @@
 # overlays/default.nix
-[]
+[
+  # Example overlay: override hello
+  (final: prev: {
+    hello = prev.hello.overrideAttrs (old: {
+      name = "hello-drzzln";
+    });
+  })
+
+  # You can add more overlays here
+]
--- parts/overlays.nix
+++ parts/overlays.nix
@@ -1,11 +1,20 @@
 # parts/overlays.nix
-{inputs, ...}: {
-  flake.overlays =
-    builtins.attrValues inputs.sops-nix.overlays
-    ++ import ../overlays
+{inputs, ...}: let
+  myOverlays =
+    [
+      # Optional: pick specific overlay from sops-nix
+      inputs.sops-nix.overlays.default
+    ]
+    ++ import ../overlays # <- imports the list you defined above
     ++ [
+      # Inline extra overlay (optional)
       (final: prev: {
-        # extra custom packages here
+        # custom = ...;
       })
     ];
+in {
+  flake.overlays = {
+    combined = final: prev:
+      builtins.foldl' (acc: o: acc // o final prev) {} myOverlays;
+  };
 }
