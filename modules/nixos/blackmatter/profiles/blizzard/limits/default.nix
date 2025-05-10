6d8d78afee70329f5352fbdf857411e4e3ad40a3
luis <luis@pleme.io>
2025-05-10 08:40:46 -0300

    init

--- modules/nixos/blackmatter/profiles/blizzard/limits/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/limits/default.nix
@@ -1,64 +1,65 @@
-{...}:{
+# modules/nixos/blackmatter/profiles/blizzard/limits/default.nix
+{...}: {
   security.pam.loginLimits = [
     {
-      domain = "*";
-      type = "soft";
-      item = "nofile";
       value = "1048576";
-    }
-    {
-      domain = "*";
-      type = "hard";
       item = "nofile";
+      type = "soft";
+      domain = "*";
+    }
+    {
       value = "1048576";
+      item = "nofile";
+      type = "hard";
+      domain = "*";
     }
     {
-      domain = "*";
-      type = "soft";
-      item = "nproc";
       value = "65536";
+      item = "nproc";
+      type = "soft";
+      domain = "*";
+    }
+    {
+      value = "65536";
+      item = "nproc";
+      type = "hard";
+      domain = "*";
+    }
+    {
+      value = "unlimited";
+      item = "stack";
+      type = "soft";
+      domain = "*";
+    }
+    {
+      value = "unlimited";
+      item = "stack";
+      type = "hard";
+      domain = "*";
+    }
+    {
+      value = "unlimited";
+      item = "memlock";
+      type = "soft";
+      domain = "*";
     }
     {
       domain = "*";
       type = "hard";
-      item = "nproc";
-      value = "65536";
-    }
-    {
-      domain = "*";
-      type = "soft";
-      item = "stack";
-      value = "unlimited";
-    }
-    {
-      domain = "*";
-      type = "hard";
-      item = "stack";
-      value = "unlimited";
-    }
-    {
-      domain = "*";
-      type = "soft";
       item = "memlock";
       value = "unlimited";
     }
     {
-      domain = "*";
-      type = "hard";
-      item = "memlock";
       value = "unlimited";
-    }
-    {
-      domain = "*";
       type = "soft";
+      domain = "*";
       item = "rss";
-      value = "unlimited";
     }
     {
-      domain = "*";
-      type = "hard";
-      item = "rss";
       value = "unlimited";
+      type = "hard";
+      domain = "*";
+      item = "rss";
     }
   ];
 }
