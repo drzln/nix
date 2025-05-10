1d5f6853bef378e38c2bb3d2346f1dad5028e88f
luis <luis@pleme.io>
2025-05-10 10:57:09 -0300

    init

---
+++ modules/home-manager/blackmatter/components/nvim/groups/lsp/utils.lua
@@ -0,0 +1,18 @@
+local M = {}
+M.exclude_servers = require("lsp.exclude_list")
+M.configure_only = {
+	"rust_analyzer",
+	"ruby_lsp",
+	"ts_ls",
+	"nixd",
+}
+function M.is_excluded(server)
+	return vim.tbl_contains(M.exclude_servers, server)
+end
+function M.is_configure_only(server)
+	return vim.tbl_contains(M.configure_only, server)
+end
+function M.merge_configs(base, overrides)
+	return vim.tbl_deep_extend("force", {}, base, overrides)
+end
+return M
