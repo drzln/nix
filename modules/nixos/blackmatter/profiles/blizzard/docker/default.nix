52d9a0e3dea7100521162bad7aa6ced4b16c8ebb
luis <luis@pleme.io>
2025-05-10 08:35:37 -0300

    init

--- modules/nixos/blackmatter/profiles/blizzard/docker/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/docker/default.nix
@@ -1,11 +1,11 @@
-{lib, ...}: {
+# modules/nixos/blackmatter/profiles/blizzard/docker.nix
+{...}: {
   systemd.services.docker.serviceConfig = {
     LimitNOFILE = "1048576";
     LimitNPROC = "65536";
     LimitSTACK = "infinity";
     LimitMEMLOCK = "infinity";
   };
-
   systemd.user.services.docker.serviceConfig = {
     # competes with virtualisation values
     # LimitNOFILE = 1048576;
@@ -13,14 +13,12 @@
     LimitSTACK = "infinity";
     LimitMEMLOCK = "infinity";
   };
-
   systemd.extraConfig = ''
     DefaultLimitNOFILE=1048576
     DefaultLimitNPROC=65536
     DefaultLimitSTACK=infinity
     DefaultLimitMEMLOCK=infinity
   '';
-
   environment.etc."docker/daemon.json".text = ''
     {
       "default-ulimits": {
