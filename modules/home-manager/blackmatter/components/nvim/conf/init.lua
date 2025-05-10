bd3871a55764580612ccc99b464415b4e31438b0
luis <luis@pleme.io>
2025-05-10 12:13:00 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/conf/init.lua
+++ modules/home-manager/blackmatter/components/nvim/conf/init.lua
@@ -1,24 +1,8 @@
--- Save the original notify function
-local orig_notify = vim.notify
 
--- Override the global notify
-vim.notify = function(msg, level, opts)
-	if type(msg) == "string" and msg:match("vim.lsp.util.parse_snippet()") and msg:match("deprecated") then
-		return
-	end
-	orig_notify(msg, level, opts)
-end
-
--- Load module loader
-local utils = require("utils")
-
--- Expand ~ and load all includes
--- local lua_home = vim.fn.expand("~/.config/nvim/lua/includes")
--- utils.load_files(lua_home)
-require('includes.common').setup()
-require('includes.treesitter').setup()
-require('includes.keybindings').setup()
-require('includes.formatting').setup()
-require('includes.completion').setup()
-require('includes.lsp').setup()
-require('includes.theming').setup()
+require("includes.common").setup()
+require("includes.treesitter").setup()
+require("includes.keybindings").setup()
+require("includes.formatting").setup()
+require("includes.completion").setup()
+require("includes.lsp").setup()
+require("includes.theming").setup()
--- modules/home-manager/blackmatter/components/nvim/groups/common/init.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/common/init.lua
@@ -1,84 +1,94 @@
 local M = {}
 function M.setup()
-  -- set leader key
-  vim.g.mapleader = ","
+	-- Save the original notify function
+	local orig_notify = vim.notify
 
-  -- highlight search results
-  vim.opt.hlsearch = true
+	-- Override the global notify
+	vim.notify = function(msg, level, opts)
+		if type(msg) == "string" and msg:match("vim.lsp.util.parse_snippet()") and msg:match("deprecated") then
+			return
+		end
+		orig_notify(msg, level, opts)
+	end
+	-- set leader key
+	vim.g.mapleader = ","
 
-  -- customize file finding
-  vim.opt.path:remove '/usr/include'
-  vim.opt.path:append '**'
-  vim.cmd [[set path=.,,,$PWD/**]]
-  vim.opt.wildignorecase = true
-  vim.opt.wildignore:append '**/node_modules/*'
-  vim.opt.wildignore:append '**/.git/*'
-  vim.opt.wildignore:append '**/build/*'
+	-- highlight search results
+	vim.opt.hlsearch = true
 
-  -- highlight on yank
-  vim.cmd [[
+	-- customize file finding
+	vim.opt.path:remove("/usr/include")
+	vim.opt.path:append("**")
+	vim.cmd([[set path=.,,,$PWD/**]])
+	vim.opt.wildignorecase = true
+	vim.opt.wildignore:append("**/node_modules/*")
+	vim.opt.wildignore:append("**/.git/*")
+	vim.opt.wildignore:append("**/build/*")
+
+	-- highlight on yank
+	vim.cmd([[
     augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
     augroup end
-  ]]
+  ]])
 
-  -- remember cursor position
-  vim.cmd [[
+	-- remember cursor position
+	vim.cmd([[
     autocmd BufReadPost *
        \ if line("'\"") > 0 && line("'\"") <= line("$") |
        \   exe "normal! g`\"" |
        \ endif
-  ]]
+  ]])
 
-  -- Time in milliseconds to wait for a mapped sequence to complete.
-  vim.opt.timeoutlen = 300
+	-- Time in milliseconds to wait for a mapped sequence to complete.
+	vim.opt.timeoutlen = 300
 
-  -- line number settings
-  vim.opt.relativenumber = true
+	-- line number settings
+	vim.opt.relativenumber = true
 
-  -- Set the number of spaces a tab character represents
-  vim.opt.tabstop = 2
+	-- Set the number of spaces a tab character represents
+	vim.opt.tabstop = 2
 
-  -- Set the number of spaces to use for each step of (auto)indent
-  vim.opt.shiftwidth = 2
+	-- Set the number of spaces to use for each step of (auto)indent
+	vim.opt.shiftwidth = 2
 
-  -- Use spaces instead of tabs
-  vim.opt.expandtab = true
+	-- Use spaces instead of tabs
+	vim.opt.expandtab = true
 
-  -- required for theming but cannot be in theming
-  vim.opt.termguicolors = true
+	-- required for theming but cannot be in theming
+	vim.opt.termguicolors = true
 
-  -- telescope
+	-- telescope
 	-- require('telescope').setup();
 
 	-- require('telescope').load_extension('projects')
-  -- projects
+	-- projects
 	-- require('project_nvim').setup {
 	-- 	manual_mode = false,
 	-- 	detection_methods = { 'lsp', 'pattern' },
 	-- 	patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
 	-- };
 
-  -- lualine
-	require('lualine').setup({
+	-- lualine
+	require("lualine").setup({
 		options = {
-			theme = 'nord',
-			section_separators = { '', '' },
-			component_separators = { '', '' },
+			theme = "nord",
+			section_separators = { "", "" },
+			component_separators = { "", "" },
 			icons_enabled = true,
 		},
 		sections = {
-			lualine_a = { 'mode' },
-			lualine_b = { 'branch', 'diff', 'diagnostics' },
-			lualine_c = { 'filename' },
-			lualine_x = { 'encoding', 'fileformat', 'filetype' },
-			lualine_y = { 'progress' },
-			lualine_z = { 'location' }
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
 		},
-	});
+	})
 
-  -- indent
+	-- indent
 	-- require('indent_blankline').setup {
 	-- 	char = '▏',
 	-- 	show_trailing_blankline_indent = false,
