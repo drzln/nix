0c3c5c6d2da78e308c3b36ff17cfeaf22b48d565
luis <luis@pleme.io>
2025-05-10 08:49:35 -0300

    init

--- modules/nixos/blackmatter/profiles/blizzard/nix/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/nix/default.nix
@@ -1,19 +1,18 @@
-{ pkgs, ... }: {
+# modules/nixos/blackmatter/profiles/blizzard/nix/default.nix
+{pkgs, ...}: {
   nix = {
+    gc = {automatic = false;};
     extraOptions = ''
       experimental-features = nix-command flakes
       min-free = ${toString (1024 * 1024 * 1024)}
       max-free = ${toString (4096 * 1024 * 1024)}
     '';
-    gc = { automatic = false; };
     settings = {
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      substituters = ["https://hyprland.cachix.org"];
+      auto-optimise-store = true;
       keep-derivations = true;
       keep-outputs = true;
-      auto-optimise-store = true;
-      substituters = [
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
     package = pkgs.nixVersions.stable;
   };
