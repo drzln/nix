aa090ec60ba6814c1e973546067ba7af5f5c9793
drzln <drzzln@protonmail.com>
2025-05-11 06:17:43 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/formatting/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/formatting/default.nix
@@ -1,30 +1,27 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
 }:
 with lib; let
   cfg = config.blackmatter.components.nvim.plugin.groups.formatting;
   common = import ../../common;
-  configPath = "${common.includesPath}/formatting/init.lua";
+  configPath = "${common.includesPath}/formatting";
 in {
   options.blackmatter.components.nvim.plugin.groups.formatting = {
     enable = mkEnableOption "manage formatting";
   };
-
   imports = [
     ../../plugins/stevearc/conform.nvim
   ];
-
   config = mkMerge [
     (
       mkIf cfg.enable
       {
         home.packages = with pkgs; [rustfmt taplo shfmt php83Packages.php-cs-fixer];
-        home.file."${configPath}".source = ./init.lua;
+        home.file."${configPath}/init.lua".source = ./init.lua;
         blackmatter.components.nvim.plugins = {
-          # formatting framework
           stevearc."conform.nvim".enable = true;
         };
       }
