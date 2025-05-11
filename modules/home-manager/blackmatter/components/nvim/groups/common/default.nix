ae035e7e94edde0e35a5755719b2625fc0e292a1
drzln <drzzln@protonmail.com>
2025-05-11 06:01:05 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/common/default.nix
+++ modules/home-manager/blackmatter/components/nvim/groups/common/default.nix
@@ -6,7 +6,7 @@
 with lib; let
   cfg = config.blackmatter.components.nvim.plugin.groups.common;
   common = import ../../common;
-  configPath = "${common.includesPath}/common/init.lua";
+  configPath = "${common.includesPath}/common";
 in {
   options.blackmatter.components.nvim.plugin.groups.common = {
     enable = mkEnableOption "plugins that should always be included";
@@ -19,7 +19,10 @@ in {
     (
       mkIf cfg.enable
       {
-        home.file."${configPath}".source = ./init.lua;
+        home.file."${configPath}/init.lua".source = ./init.lua;
+        home.file."${configPath}/notify.lua".source = ./notify.lua;
+        home.file."${configPath}/settings.lua".source = ./settings.lua;
+        home.file."${configPath}/autocmds.lua".source = ./autocmds.lua;
         blackmatter.components.nvim.plugins = {
           nvim-lua."plenary.nvim".enable = true;
           nvim-tree.nvim-web-devicons.enable = true;
