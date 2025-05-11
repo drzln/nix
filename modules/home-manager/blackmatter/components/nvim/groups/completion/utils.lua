b2f12efd5091b0f0ca5c7b006d06fede14735a5f
drzln <drzzln@protonmail.com>
2025-05-11 06:06:38 -0300

    init

--- modules/home-manager/blackmatter/components/nvim/groups/completion/utils.lua
+++ modules/home-manager/blackmatter/components/nvim/groups/completion/utils.lua
@@ -5,7 +5,10 @@ function M.has_words_before()
 	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 		return false
 	end
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local line = cursor[1]
+	local col = cursor[2]
+
 	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
