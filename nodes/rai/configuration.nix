d72dc05fcac6bbe88b28a7e8d7dda6c4f0261489
drzzln <drzzln@protonmail.com>
2025-05-12 06:26:49 -0300

    init

--- nodes/rai/configuration.nix
+++ nodes/rai/configuration.nix
@@ -1,25 +1,5 @@
 # nodes/rai/configuration.nix
-{
-  inputs,
-  # pkgs,
-  ...
-}: let
-  # sudo-users-common = {
-  #   shell = pkgs.zsh;
-  #   isNormalUser = true;
-  #   extraGroups = [
-  #     "networkmanager"
-  #     "libvirtd"
-  #     "docker"
-  #     "podman"
-  #     "wheel"
-  #     "audio"
-  #     "video"
-  #   ];
-  #   packages = with pkgs; [
-  #     home-manager
-  #   ];
-  # };
+{inputs, ...}: let
 in {
   system.stateVersion = "24.05";
   nixpkgs.config.allowUnfree = true;
@@ -29,23 +9,5 @@ in {
     inputs.nix-kubernetes.nixosModules.kubernetes
     ./base-configuration.nix
     ./base-hardware.nix
-    # ./kubernetes
-    # ./secrets.nix
   ];
-  # blackmatter.profiles.blizzard.enable = true;
-  # makes /bin/bash work
-  # services.envfs.enable = true;
-  # environment.systemPackages = with pkgs; [
-  #   kubectl
-  #   runc
-  # ];
-  # users.users.luis =
-  #   {
-  #     uid = 1001;
-  #     description = "luis";
-  #   }
-  #   // sudo-users-common;
-  # security.sudo.extraConfig = ''
-  #   luis ALL=(ALL) NOPASSWD:ALL
-  # '';
 }
