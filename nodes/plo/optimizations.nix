1a6748846df8c9f7b317ee1cd17d8fa37d582e66
drzln <drzzln@protonmail.com>
2025-05-12 09:38:13 -0300

    init

--- nodes/plo/optimizations.nix
+++ nodes/plo/optimizations.nix
@@ -1,3 +1,16 @@
 {pkgs, ...}: {
+  # use a real-time linux kernel
   boot.kernelPackages = pkgs.linuxPackages_rt;
+
+  # Ensure CPU governor is "performance" for max speed
+  powerManagement.cpuFreqGovernor = "performance";
+  # (If using intel_pstate, this ensures CPU stays in performance mode)
+
+  # Udev rule to set I/O scheduler for NVMe
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="nvme0n1", ATTR{queue/scheduler}="none"
+  '';
+
+  # stop nvme drives from sleeping
+  boot.kernelParams = ["nvme_core.default_ps_max_latency_us=0"];
 }
