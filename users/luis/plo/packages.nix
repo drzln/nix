c3c11fa91afd37c92ca902931f1a1df1684a9a9b
drzzln <drzzln@protonmail.com>
2025-05-12 07:41:20 -0300

    init

--- users/luis/plo/packages.nix
+++ users/luis/plo/packages.nix
@@ -4,14 +4,11 @@
     (pkgs.stdenv.mkDerivation {
       pname = "connect-vpn-pinger";
       version = "1.0.0";
-
       src = pkgs.writeScript "connect-vpn-pinger.sh" ''
         #!/usr/bin/env bash
         sudo openconnect --protocol=gp --mtu=1200 pan.corp.pinger.com
       '';
-
       phases = ["installPhase"];
-
       installPhase = ''
         mkdir -p $out/bin
         install -m755 "$src" $out/bin/connect-vpn-pinger
