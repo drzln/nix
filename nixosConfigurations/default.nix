c438f51e0da32c81015deccd62442d9a3d3557d4
drzzln <drzzln@protonmail.com>
2025-05-12 05:54:26 -0300

    init

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -11,4 +11,13 @@
       ../nodes/plo/configuration.nix
     ];
   };
+
+  rai = nixpkgs.lib.nixosSystem {
+    inherit specialArgs;
+    system = "x86_64-linux";
+    modules = [
+      specialArgs.inputs.sops-nix.nixosModules.sops
+      ../nodes/rai/configuration.nix
+    ];
+  };
 }
