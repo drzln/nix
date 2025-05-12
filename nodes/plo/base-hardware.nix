8eda6c69a4b821b0d8d4f011fa2ec3c190cd5430
drzln <drzzln@protonmail.com>
2025-05-12 09:40:24 -0300

    init

--- nodes/plo/base-hardware.nix
+++ nodes/plo/base-hardware.nix
@@ -2,9 +2,9 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 {
+  modulesPath,
   config,
   lib,
-  modulesPath,
   ...
 }: {
   imports = [
