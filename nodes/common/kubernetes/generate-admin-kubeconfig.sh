8b82392a1f0ff0e4d2917eaf3878d0a946a7feae
drzln <drzzln@protonmail.com>
2025-05-16 13:51:49 -0300

    init

---
+++ nodes/common/kubernetes/generate-admin-kubeconfig.sh
@@ -0,0 +1,25 @@
+set -euo pipefail
+export KUBECONFIG=/run/secrets/kubernetes/configs/admin/kubeconfig
+SERVER=kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+CA_CERT=kubectl config view --raw -o jsonpath='{.clusters[0].cluster.certificate-authority-data}')
+TOKEN=kubectl get secret $(${pkgs.kubernetes}/bin/kubectl get sa admin -o jsonpath='{.secrets[0].name}') -o jsonpath='{.data.token}' | base64 --decode)
+cat > admin.kubeconfig <<EOF
+apiVersion: v1
+kind: Config
+clusters:
+  - name: kubernetes
+    cluster:
+      certificate-authority-data: $CA_CERT
+      server: $SERVER
+contexts:
+  - name: admin@kubernetes
+    context:
+      cluster: kubernetes
+      user: admin
+current-context: admin@kubernetes
+users:
+  - name: admin
+    user:
+      token: $TOKEN
+EOF
+echo "[✓] Generated admin.kubeconfig successfully."
