72eaa65f5b70885ba4f65e649bdf721e5d6a93b6
drzln <drzzln@protonmail.com>
2025-05-17 15:08:38 -0300

    init

--- nodes/common/kubernetes/secrets.nix
+++ nodes/common/kubernetes/secrets.nix
@@ -10,4 +10,13 @@
       path = "/var/lib/blackmatter/pki/admin.token";
     };
   };
+  sops.secrets = {
+    "fluxcd/kube-clusters/pat" = {
+      mode = "0444";
+      owner = "root";
+      group = "root";
+      sopsFile = ../../../secrets.yaml;
+      path = "/var/lib/blackmatter/pki/kube-clusters.pat";
+    };
+  };
 }
