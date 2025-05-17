04ff76baf3d5c4291e62a5e9a3f2e797e429d364
drzln <drzzln@protonmail.com>
2025-05-17 12:14:17 -0300

    init

--- nodes/common/kubernetes/default.nix
+++ nodes/common/kubernetes/default.nix
@@ -5,5 +5,13 @@
   blackmatter.components.kubernetes = {
     enable = true;
     role = "single";
+    fluxcd.enable = true;
+    fluxcd.owner = "drzzln";
+    fluxcd.repo = "kube-clusters";
+    fluxcd.branch = "main";
+    fluxcd.path = "clusters/rai";
+    fluxcd.patFile = "/run/secrets/kubernetes/fluxcd/pat";
+    fluxcd.runAtBoot = false;
+    fluxcd.lockFile = "/var/lib/blackmatter/fluxcd-bootstrap.lock";
   };
 }
